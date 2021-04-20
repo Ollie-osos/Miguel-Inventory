4dad47dc4120bd0579a890fae7d23451434689a3
Ollie-osos <m.oliver.smith@gmail.com>
2021-04-20 19:08:43 +0100

    trial home page fix

--- wp-content/themes/MiguelInventory/page-home.php
+++ wp-content/themes/MiguelInventory/page-home.php
@@ -22,15 +22,15 @@ get_sidebar(); ?>
 <div id="page" class="main col-sm-10 col-md-9">
 
 	<h5>Oeuvres</h5>
-	<?php echo do_shortcode('[posts_table rows_per_page="3" post_limit="5" post_type=oeuvre columns="image: Photo,title:Title,cf:creation_date:Date,category:Catégorie,tags:Série,cf:unique_work_code:Numéro d\'inventaire" lazy_load="true"]'); ?>
+	<?php echo do_shortcode('[posts_table pagination="false" rows_per_page="3" post_limit="5" post_type=oeuvre columns="image: Photo,title:Title,cf:creation_date:Date,category:Catégorie,tags:Série,cf:unique_work_code:Numéro d\'inventaire" lazy_load="true"]'); ?>
 
-	<h5>Expositions</h5>
+	<!-- <h5>Expositions</h5>
 
-	<?php echo do_shortcode('[posts_table rows_per_page="3" post_limit="5" post_type=exposition columns="image: Photo,title:Title,cf:city:Ville,cf:start_date:Dates,cf:unique_exhibition_code:Code exposition" lazy_load="true"]'); ?>
+	<?php // echo do_shortcode('[posts_table rows_per_page="3" post_limit="5" post_type=exposition columns="image: Photo,title:Title,cf:city:Ville,cf:start_date:Dates,cf:unique_exhibition_code:Code exposition" lazy_load="true"]'); ?>
 
 	<h5>Réferences Bibliographiques</h5>
 
-	<?php echo do_shortcode('[posts_table rows_per_page="3" post_limit="5" post_type=bibliographies columns="image: Photo,cf:author:Author,title:Title,cf: publication_date:Date,cf:editor:Editeur,cf:unique_bibliography_code:Code référence" lazy_load="true"]'); ?>
+	<?php // echo do_shortcode('[posts_table rows_per_page="3" post_limit="5" post_type=bibliographies columns="image: Photo,cf:author:Author,title:Title,cf: publication_date:Date,cf:editor:Editeur,cf:unique_bibliography_code:Code référence" lazy_load="true"]'); ?> -->
 
 </div>
 
