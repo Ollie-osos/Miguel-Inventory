ff158a88843edb0527e74f9c01ea119934c912c6
Ollie-osos <m.oliver.smith@gmail.com>
2021-06-07 16:11:49 +0100

    ON off for single pages

--- wp-content/themes/MiguelInventory/single-exposition.php
+++ wp-content/themes/MiguelInventory/single-exposition.php
@@ -90,24 +90,37 @@
       <h5>Lieu</h5>
     </div>
 
-    <div class="col-md-4"><strong>Adresse</strong></div>
-    <div class="col-md-8"><span><?php the_field('address'); ?></span></div>
-    <div class="col-md-4"><strong>Structure/Institutions</strong></div>
-    <div class="col-md-8"><span><?php the_field('structure'); ?></span></div>
+    <?php if (get_field('address')) { ?>
+      <div class="col-md-4"><strong>Adresse</strong></div>
+      <div class="col-md-8"><span><?php the_field('address'); ?></span></div>
+    <?php } ?>
+    <?php if (get_field('structure')) { ?>
+      <div class="col-md-4"><strong>Structure/Institutions</strong></div>
+      <div class="col-md-8"><span><?php the_field('structure'); ?></span></div>
+    <?php } ?>
+    <?php if (get_field('city')) { ?>
     <div class="col-md-4"><strong>Ville</strong></div>
     <div class="col-md-8"><span><?php the_field('city'); ?></span></div>
+    <?php } ?>
+    <?php if (get_field('country')) { ?>
     <div class="col-md-4"><strong>Pays</strong></div>
     <div class="col-md-8"><span><?php the_field('country'); ?></span></div>
-
+    <?php } ?>
+    
+    <?php if (get_field('description')) { ?>
     <div class="col-md-4"><strong>Description</strong></div>
     <div class="col-md-8"><span><?php the_field('description'); ?></span></div>
-
+    <?php } ?>
+    
+    <?php if (get_field('curator')) { ?>
     <div class="col-md-4"><strong>Curator</strong></div>
     <div class="col-md-8"><span><?php the_field('curator'); ?></span></div>
+    <?php } ?>
 
+    <?php if (get_field('url')) { ?>
     <div class="col-md-4"><strong>URL</strong></div>
     <div class="col-md-8"><span><?php the_field('url'); ?></span></div>
-
+    <?php } ?>
     <?php
     if (have_rows('affiliated_works')) :
     ?>
@@ -150,145 +163,7 @@
       <div class="col-md-8"><span><?php the_field('private_notes'); ?></span></div>
     <?php } ?>
 
-
-
-    <div class="columns col-md-12">
-      <h2>Mediateque</h2>
-    </div>
-
-    <div class="col-md-12">
-      <h5><strong>Documents</strong></h5>
-
-
-      <div class="row">
-
-        <?php if (have_rows('repeater_docs')) : ?>
-
-
-          <?php while (have_rows('repeater_docs')) : the_row(); ?>
-
-
-
-            <?php // load selected file (from post)
-            $file = get_sub_field('docs');
-
-            if ($file) :
-
-              // load selected thumbnail (from attachment)
-              $thumbnail = get_sub_field('docs', $file['ID']);
-
-            ?>
-              <div class="file col-md-3">
-                <a target='_blank' href="<?php echo $file['url']; ?>" download=<?php echo $file['filename'];  ?>>
-                  <?php if ($thumbnail) : ?>
-                    <img class="thumbnail" src="../../wp-includes/images/media/document.png" alt="<?php echo $thumbnail['alt']; ?>" />
-                  <?php endif; ?>
-                  <br>
-                  <span><?php echo $file['filename']; ?></span>
-                </a>
-              </div>
-            <?php endif; ?>
-
-
-
-          <?php endwhile; ?>
-
-
-        <?php endif; ?>
-      </div>
-    </div>
-
-
-    <div class="col-md-12">
-      <h5><strong>Images</strong></h5>
-
-
-      <div class="row">
-
-        <?php // load selected file (from post)
-
-        $images = get_field('images');
-
-        if ($images) : ?>
-          <?php foreach ($images as $image) : ?>
-            <div class="col-md-3">
-              <a target='_blank' href="<?php echo esc_url($image['url']); ?>">
-                <img src="<?php echo esc_url($image['sizes']['medium']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-              </a>
-              <p><?php echo esc_html($image['caption']); ?></p>
-            </div>
-          <?php endforeach; ?>
-        <?php endif; ?>
-
-
-
-      </div>
-    </div>
-
-    <div class="col-md-12">
-      <h5><strong>Audios</strong></h5>
-
-
-      <div class="row">
-
-        <?php if (have_rows('repeater_audio')) : ?>
-
-
-          <?php while (have_rows('repeater_audio')) : the_row(); ?>
-
-
-
-            <?php // load selected file (from post)
-            $file = get_sub_field('audio');
-
-            if ($file) :
-
-              // load selected thumbnail (from attachment)
-              $thumbnail = get_sub_field('audio', $file['ID']);
-
-            ?>
-              <div class="file col-md-3">
-                <a target='_blank' href="<?php echo $file['url']; ?>" download=<?php echo $file['filename'];  ?>>
-                  <?php if ($thumbnail) : ?>
-                    <img class="thumbnail" src="../../wp-includes/images/media/audio.png" alt="<?php echo $thumbnail['alt']; ?>" />
-                  <?php endif; ?>
-                  <br>
-                  <span><?php echo $file['filename']; ?></span>
-                </a>
-              </div>
-            <?php endif; ?>
-
-
-
-          <?php endwhile; ?>
-
-
-        <?php endif; ?>
-      </div>
-    </div>
-
-
-    <?php if (the_field('vimeo') && is_user_logged_in()) { ?>
-
-      <div class="col-md-12">
-        <h5>Vidéos</h5>
-      </div>
-      <div class="col-md-12">
-
-        <iframe src="<?php the_field('vimeo'); ?>?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
-        <p><a href="<?php the_field('vimeo'); ?>"></a></p>
-      </div>
-    <?php } ?>
-
-
-
-
-
-
   </div>
-
-
-
 
   <?php get_template_part('/template-parts/media.tpl'); ?>
 
